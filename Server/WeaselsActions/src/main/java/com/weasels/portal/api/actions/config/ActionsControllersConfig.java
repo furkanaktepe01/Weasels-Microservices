042aead35a4b2f873cd100d8c843265f6deed9cc
furkanaktepe01 <7.furkanaktepe@gmail.com>
2022-02-06 14:44:52 +0300

    add actions config

--- Server/WeaselsActions/src/main/java/com/weasels/portal/api/actions/config/ActionsControllersConfig.java
+++ Server/WeaselsActions/src/main/java/com/weasels/portal/api/actions/config/ActionsControllersConfig.java
@@ -1,4 +1,4 @@
-package com.weasels.portal.api.actions;
+package com.weasels.portal.api.actions.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
